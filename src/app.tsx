4010462dbbc6846c96d9e2d7ebfd12124dce38f7
mauroj <mauroj@siainteractive.com>
2025-05-18 13:33:04 +0200

    chore: init solidjs start with bun

---
+++ src/app.tsx
@@ -0,0 +1,25 @@
+import { MetaProvider, Title } from "@solidjs/meta";
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { Suspense } from "solid-js";
+import "./app.css";
+
+export default function App() {
+  return (
+    <Router
+      root={props => (
+        <MetaProvider>
+          <Title>Media Player Basic</Title>
+          {/* <nav>
+            <a href="/">Index</a>
+            <a href="/about">About</a>
+          </nav> */}
+          
+          <Suspense>{props.children}</Suspense>
+        </MetaProvider>
+      )}
+    >
+      <FileRoutes />
+    </Router>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Title } from "@solidjs/meta";
+// import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Media Player</Title>
+      
+      {/* <Counter /> */}
+
+    </main>
+  );
+}
